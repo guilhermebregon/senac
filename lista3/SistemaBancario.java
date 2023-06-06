9673a97db3d5d9f73b97b22c7399ab4c8fedcfcb
profKalil <84177112+profKalil@users.noreply.github.com>
2023-06-06 18:53:19 -0300

    Update SistemaBancario.java
    Método main() ficou menor, com alteração do método menu( ), passando validaSenha() por parâmetro.

--- lista3/SistemaBancario.java
+++ lista3/SistemaBancario.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 public class SistemaBancario {
 	public static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
-		if (validaSenha()) {
-			menu();
+		if (menu(validaSenha())) {
 		} else {
 			System.out.println("Acesso bloqueado");
 		}
 		sc.close();
 	}
+
 	public static boolean validaSenha() {
 		final String senhaSistema = "123xyz";
 		int tentativas = 3;
@@ -23,83 +23,91 @@ public class SistemaBancario {
 			} else {
 				tentativas--;
 				System.out.println("Senha invalida. Voce tem mais " + tentativas + " tentativas");
-				if (tentativas==0) {
+				if (tentativas == 0) {
 					return false;
 				}
 			}
 		}
 	}
-	public static boolean menu() {
-		int operacao = 0;		
-		boolean sair = false;
-		double saldo = 0.0, limite = 1000.0, valorOperacao = 0.0;
-		while (true) {
-			System.out.println("\nSeu saldo atual-> " + saldo + " + " + limite + " de limite");
-			if (sair==true) {
-				System.out.println("Deseja sair? Sim ou Não");
-				System.out.print("Opcao - >");
-				String opcao = sc.next();
-				String Sim="Sim", sim="sim";
-				if (opcao.equals(sim) || opcao.equals(Sim)) {
-					return false;
-				} 
-			}
-			operacao = 0;
-			// public static int menu2()
-			do {
-				System.out.print("\nEscolha uma operacao:" + "\n1 - debito \n2 - credito" + "\nOpcao -> ");
-				operacao = sc.nextInt();
-			} while (!(operacao == 1 || operacao == 2));
-			
-			// public static double saque();
-			while (valorOperacao <= 0.0) {
-				System.out.print("\nValor -> ");
-				valorOperacao = sc.nextDouble();
-			}
-			if (operacao == 1) {
-				if (valorOperacao <= (saldo + limite)) {
-					saldo -= valorOperacao;
-					if (saldo < 0) {
-						limite = limite + saldo;
-						if (limite < -1000) {
-							saldo += valorOperacao;
-							limite = 1000;
-							System.out.println("Limite insuficiente. Fale com seu gerente");
+
+	public static boolean menu(boolean executa) {
+		if (executa) {
+			int operacao = 0;
+			boolean sair = false;
+			double saldo = 0.0, limite = 1000.0, valorOperacao = 0.0;
+			while (true) {
+				System.out.println("\nSeu saldo atual-> " + saldo + " + " + limite + " de limite");
+				if (sair == true) {
+					System.out.println("Deseja sair? Sim ou Não");
+					System.out.print("Opcao - >");
+					String opcao = sc.next();
+					String Sim = "Sim", sim = "sim";
+					if (opcao.equals(sim) || opcao.equals(Sim)) {
+						return true;
+					}
+				}
+				operacao = 0;
+				// public static int menu2()
+				do {
+					System.out.print("\nEscolha uma operacao:" + "\n1 - debito \n2 - credito" + "\nOpcao -> ");
+					operacao = sc.nextInt();
+				} while (!(operacao == 1 || operacao == 2));
+
+				// public static double saque();
+				while (valorOperacao <= 0.0) {
+					System.out.print("\nValor -> ");
+					valorOperacao = sc.nextDouble();
+				}
+				if (operacao == 1) {
+					if (valorOperacao <= (saldo + limite)) {
+						saldo -= valorOperacao;
+						if (saldo < 0) {
+							limite = limite + saldo;
+							if (limite < -1000) {
+								saldo += valorOperacao;
+								limite = 1000;
+								System.out.println("Limite insuficiente. Fale com seu gerente");
+							}
 						}
+					} else {
+						System.out.println("Limite insuficiente. Fale com seu gerente");
 					}
-				} else {
-					System.out.println("Limite insuficiente. Fale com seu gerente");
-				}
-				valorOperacao = 0.0;
-				sair = true;
-				
-				// public static double deposito();
-			} else if (operacao == 2) {
-				if (limite < 1000) {
-					limite += valorOperacao;
-					saldo += valorOperacao;
-					if (limite > 1000) {
-						saldo = limite - 1000;
-						limite = 1000;
+					valorOperacao = 0.0;
+					sair = true;
+
+					// public static double deposito();
+				} else if (operacao == 2) {
+					if (limite < 1000) {
+						limite += valorOperacao;
+						saldo += valorOperacao;
+						if (limite > 1000) {
+							saldo = limite - 1000;
+							limite = 1000;
+						}
+					} else {
+						saldo += valorOperacao;
 					}
-				} else {
-					saldo += valorOperacao;
+					valorOperacao = 0.0;
+					sair = true;
 				}
-				valorOperacao = 0.0;
-				sair = true;
 			}
+		} else {
+			return false;
 		}
 	}
+
 	public static double saque() {
 		// implementar
 		return 0.0;
 	}
+
 	public static double deposito() {
 		// implementar
 		return 0.0;
 	}
+
 	public static int menu2() {
-		//implemantar
+		// implemantar
 		return 0;
 	}
 }
